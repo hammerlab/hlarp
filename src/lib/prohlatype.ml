c2431ff538b6337f72195b3562d101b4e47de7f1
Leonid Rozenberg <leonidr@gmail.com>
2016-11-03 11:06:26 -0400

    Change prohlatype directory searching to be more generous

--- src/lib/prohlatype.ml
+++ src/lib/prohlatype.ml
@@ -1,14 +1,24 @@
 
 open Std
 
-let suffix = ".txt"
-let filename_regex = Re_posix.compile_pat ("([^/]+)/([^/]+)_[^/]+_(nuc|gen)" ^ suffix)
+let suffix = "(.txt)?$"
+
+(* The nuc/gen .txt are all optional at the moment, so we need the '$'
+   to bind to the end! *)
+let filename_regex =
+  Re_posix.compile_pat ("([^/]+)/([^/]+)_[^/]+(_(nuc|gen))?" ^ suffix)
 
 let allele_to_hla_class s =
   if String.get s 0 = 'D' then II else I
 
 let parse (fname, re_group) =
   let run = Re.Group.get re_group 2 in
+  let run =
+    if String.contains run '_' then
+      String.sub run 0 (String.index run '_')
+    else
+      run
+  in
   let ic = open_in fname in
   let rec loop acc =
     try
@@ -32,7 +42,7 @@ let parse (fname, re_group) =
   in
   run, ilst
 
-let scan_directory dir =
+let scan_directory (dir : string) : ((sample * info list) list) =
   let rec loop acc = function
     | [] -> acc
     | dir :: t ->
