449ca6c49fb7af09cd773b27538e962cfdf831f9
Leonid Rozenberg <leonidr@gmail.com>
2016-11-03 11:05:47 -0400

    Document functions

--- src/lib/std.ml
+++ src/lib/std.ml
@@ -4,12 +4,12 @@ open Printf
 
 module StringMap = Map.Make (struct type t = string let compare = compare end)
 
+(* Combine assocation lists, with list values, by their string keys. *)
 let join_by_fst lst =
-  List.fold_left lst ~f:(fun acc (run, inf) ->
-    match StringMap.find run acc with
-    | exception Not_found -> StringMap.add run inf acc
-    | clst -> StringMap.add run (clst @ inf) acc)
-        ~init:StringMap.empty
+  List.fold_left lst ~init:StringMap.empty ~f:(fun acc (run, inf) ->
+      match StringMap.find run acc with
+      | exception Not_found -> StringMap.add run inf acc
+      | clst -> StringMap.add run (clst @ inf) acc)
   |> StringMap.bindings
 
 let float_of_string_nanable s = try float_of_string s with Failure _ -> nan
@@ -21,6 +21,10 @@ let hla_class_to_string = function | I -> "1" | II -> "2"
 (* This type is a place holder in case we want to
    make a full ADT of MHC alleles in the future. *)
 type allele = string
+
+(** A sample (aka run) is an obvservation for which we may have multiple
+    [info]'s. *)
+type sample = string
 
 type info =
   { hla_class   : hla_class
@@ -35,4 +39,3 @@ let info_to_string { hla_class; allele; qualifier; confidence} =
 
 module InfoMap = Map.Make (struct type t = info let compare = compare end)
 
-type sample = string
